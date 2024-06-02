76c2dc7cd9ac99b738404e7e22859c055543f9e4
JohnMutemi <jmtutorsalp@gmail.com>
2024-06-02 18:58:05 +0300

    commit file_io lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,11 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', 'w') as file_obj:
+        file_obj.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as file_obj:
+        file_obj.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', 'r') as file_obj:
+        return file_obj.read()
